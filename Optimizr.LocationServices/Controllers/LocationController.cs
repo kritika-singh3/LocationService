675cf25a29d261b6f510717620a9973fa39a0bf9
kritika-singh3 <41165891+kritika-singh3@users.noreply.github.com>
2018-08-09 14:36:58 +0530

    Create LocationController.cs

---
+++ Optimizr.LocationServices/Controllers/LocationController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Optimizr.LocationServices.Controllers
+{
+    public class LocationController : Controller
+    {
+        // GET: Location
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
